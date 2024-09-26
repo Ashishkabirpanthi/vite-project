fdd8c6dc0b7c301b31b7ce570e9867799b89b550
Ashishkabirpanthi <ashishkabirpanthi@gmail.com>
2024-09-26 16:04:10 +0530

    about react veriable(usestate)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
+
 function App() {
+  // const getInfo = () => {
+  //   console.log("hello world");
+  // }
+  // const datainfo = (takeinfo) => {
+  //   console.log(takeinfo);
+  // };
+
+  let [a, seta] = useState(0);
+  const counter = () => {
+    seta(a+1);
+  };
+  console.log(a);
   return (
-    <div className="bg-blue-500 flex items-center justify-center h-screen">
-      <h1>Hello From React</h1>
-      <h2 className="text-red-500">
-        {import.meta.env.VITE_API_KEY}
-      </h2>
+    // <div>
+    //   {/* <h1>Hello From React</h1>
+    //   <h2 className="text-red-500">
+    //     {import.meta.env.VITE_API_KEY}
+    //   // </h2> */}
+    //   <button onClick={getInfo}>click me!</button>
+    //   <button onClick={() => {datainfo('hello')}}>press</button>
+    // </div>
+    <div>
+      <h1 className="ml-4">{a}</h1>
+      <button className="bg-red-500 m-2 p-1" onClick={counter}>add</button>
     </div>
   );
 }
