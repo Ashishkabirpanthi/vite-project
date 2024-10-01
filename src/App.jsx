2211873d734579f1d0dffe313d0f102c184ffa9b
Ashishkabirpanthi <ashishkabirpanthi@gmail.com>
2024-10-01 17:09:50 +0530

    two way binding

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 function App() {
+  // const [time,settime] = useState(new  Date().toLocaleTimeString());
   // const getInfo = () => {
   //   console.log("hello world");
   // }
@@ -8,11 +9,22 @@ function App() {
   //   console.log(takeinfo);
   // };
 
-  let [a, seta] = useState(0);
-  const counter = () => {
-    seta(a+1);
-  };
-  console.log(a);
+  // let [a, seta] = useState(0);
+  // const counter = () => {
+  //   seta(a+1);
+  // };
+  // console.log(a);
+  // setInterval(()=>{
+  //   settime(new  Date().toLocaleTimeString());
+  // },1000)
+  const submitHandler = (event) => {
+      event.preventDefault();
+      // console.log(event.target.username.value);
+  console.log({username})
+
+  }
+  const [username,setuser] = useState("");
+  console.log(username);
   return (
     // <div>
     //   {/* <h1>Hello From React</h1>
@@ -22,10 +34,18 @@ function App() {
     //   <button onClick={getInfo}>click me!</button>
     //   <button onClick={() => {datainfo('hello')}}>press</button>
     // </div>
-    <div>
-      <h1 className="ml-4">{a}</h1>
-      <button className="bg-red-500 m-2 p-1" onClick={counter}>add</button>
-    </div>
-  );
+    // <div>
+    //   <h1 className="ml-4">{a}</h1>
+    //   <button className="bg-red-500 m-2 p-1" onClick={counter}>add</button>
+    // </div>
+  <div>
+    {/* <h1 className="flex justify-center align-center">{time}</h1> */}
+    <form onSubmit={submitHandler}> 
+      <input onChange = { (e) => setuser(e.target.value)} value = {username} type="text" placeholder="Username" name = "username"/>
+      <button type="submit">submit</button>
+    </form>
+  </div>
+  
+   );
 }
 export default App;
